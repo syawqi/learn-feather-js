9d9ecf44d8fba2d0f1398c1671bf0bd503566df1
Syawqi Muhammad <syawqi.zone@gmail.com>
2018-01-14 23:53:52 +0800

    init

---
+++ app.js
@@ -0,0 +1,22 @@
+const feathers = require('@feathersjs/feathers');
+const app = feathers();
+
+app.use('todo', {
+  get(name){
+    return Promise.resolve(
+      {
+        name
+      }
+    )
+  }
+});
+
+function getTodo(name) {
+  const service = app.service('todo');
+  const todo = service.get(name).then(
+    message => {
+      console.log(message.name);
+  });
+}
+
+getTodo('donut');
